af15ac8305190a9241f389e300a199e588030bf4
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-10-04 15:22:24 +0100

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import HomePage from './components/pages/HomePage'
+import Login from './components/pages/Login';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App;
