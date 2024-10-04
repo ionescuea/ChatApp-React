20cdbf8a45a2fbaa74f786e5adecc8918b6ac2fb
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-10-04 15:20:50 +0100

    Add navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,35 @@
-const NavBar = () => (
-    <div className="container">
-      <h1>NavBar</h1>
-    </div>
-  )
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  export default NavBar;
+
+function NavBar(props) {
+    const [setCurrentPage] = useState('HomePage');
+
+    const handlePageChange = (page) => {
+        props.handlePageChange(page);
+        setCurrentPage(page);
+    };
+
+    return (
+        <nav className="navbar fixed-top m-0 p-0">
+            <div className="container-fluid pr-0">
+                <a className="navbar-brand m-0" href="#HomePage" onClick={() => handlePageChange('HomePage')}>
+                    <img src="/logo.jpg" alt="Logo" width="50" height="50"/>
+                </a>
+                <a className="navbar-title" href="#HomePage" onClick={() => handlePageChange('HomePage')}>ChatApp</a>
+                {/* <div className="avatar">
+                    <span className="status active"></span>
+                </div> */}
+                <button type="button" className="btn btn-outline-light">Log out</button>
+            </div>
+        </nav>
+    );
+}
+
+NavBar.propTypes = {
+    currentPage: PropTypes.string.isRequired,
+    handlePageChange: PropTypes.func.isRequired,
+};
+
+export default NavBar;
