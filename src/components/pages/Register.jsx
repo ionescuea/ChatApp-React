44f795a767c2d6c7351f9ede525c11e462a4b0bf
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-10-07 17:25:58 +0100

    Add register page

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,7 +1,27 @@
-const Register = () => (
-    <div className="container">
-      <h1>Register</h1>
-    </div>
-  )
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  export default Register;
+
+const Register = () => (
+  <section id="register">
+    <div className="container">
+      <div className="square">
+        <form>
+          <h1 className="register-title d-grid col-4 mx-auto">Register</h1>
+          <div className="mb-3">
+            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email*"></input>
+          </div>
+          <div className="mb-3">
+            <input type="username" className="form-control" id="exampleInputUsername1" placeholder="Username*"></input>
+          </div>
+          <div className="mb-3">
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password*"></input>
+          </div>
+          <div id="emailHelp" className="form-text d-grid col-9 mx-auto">Please complete all fields that are marked with *.</div>
+          <button type="submit" className="d-grid col-6 mx-auto register-button btn btn-primary">Register</button>
+        </form>
+      </div>
+    </div>
+  </section >
+);
+
+export default Register;
