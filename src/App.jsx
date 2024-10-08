74be41edd0881c79f49dd9a5fe93264ba839415c
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-10-08 17:33:42 +0100

    Add admin page design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './components/pages/HomePage'
 import Register from './components/pages/Register';
 import Login from './components/pages/Login';
 import Chat from './components/pages/Chat';
+import AdminPage from './components/pages/AdminPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -16,6 +17,7 @@ function App() {
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
           <Route path='/chat' element={<Chat />} />
+          <Route path='adminPage' element={<AdminPage />} />
         </Routes>
       </Router>
     </>
--- src/components/pages/AdminPage.jsx
+++ src/components/pages/AdminPage.jsx
@@ -1,7 +1,39 @@
-const AdminPage = () => (
-    <div className="container">
-      <h1>AdminPage</h1>
-    </div>
-  )
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  export default AdminPage;
+const AdminPage = () => (
+  <section id="adminPage">
+    <h1 className="text-center">Admin Page</h1>
+    <div className="admin container">
+      <div className="admin-row row gx-5">
+        <div className="admin-card col-lg-3 border">
+          User 1
+        </div>
+        <div className="admin-card col-lg-3 border">
+          Email
+        </div>
+        <div className="admin-card col-lg-3 border">
+          Password
+        </div>
+        <div className="admin-card col-lg-3 border">
+          Remove
+        </div>
+      </div>
+      <div className="admin-row row gx-5">
+        <div className="admin-card col-lg-3 border">
+          User 1
+        </div>
+        <div className="admin-card col-lg-3 border">
+          Email
+        </div>
+        <div className="admin-card col-lg-3 border">
+          Password
+        </div>
+        <div className="admin-card col-lg-3 border">
+          Remove
+        </div>
+      </div>
+    </div>
+  </section >
+)
+
+export default AdminPage;
