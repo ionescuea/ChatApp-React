e74b97a318ebd7884fce57a12ab11de7d4d454cb
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-10-11 15:36:57 +0100

    Add changes to local storage

--- utilities/usersLS.js
+++ utilities/usersLS.js
@@ -1,13 +1,15 @@
-const originalUsers = {
-    user1: {name: 'Anna Smith', email: 'anna@example.com', password: 'apple1', role: 'admin'},
-    user2: {name: 'John Doe', email: 'john@example.com', password: 'beer7', role: 'user'},
-    user3: {name: 'Ross Charlie', email: 'ross@example.com', password: 'root8', role: 'user'},
-    user4: {name: 'Mike Smith', email: 'mike@example.com', password: 'ginger6', role: 'user'},
-    user5: {name: 'Helena Johnson', email: 'helena@example.com', password: 'grape3', role: 'user'}
-};
+const originalUsers = [
+    {user: 'Anna Smith', email: 'anna@example.com', password: 'apple1', role: 'admin'},
+    {user: 'John Doe', email: 'john@example.com', password: 'beer7', role: 'user'},
+    {user: 'Ross Charlie', email: 'ross@example.com', password: 'root8', role: 'user'},
+    {user: 'Mike Smith', email: 'mike@example.com', password: 'ginger6', role: 'user'},
+    {user: 'Helena Johnson', email: 'helena@example.com', password: 'grape3', role: 'user'}
+];
 
 localStorage.setItem('users', JSON.stringify(originalUsers));
 
-const storedUsers = JSON.parse(localStorage.getItem('users'));
+function getStoredUsers() {
+    return JSON.parse(localStorage.getItem('users'));
+}
 
-console.log(storedUsers);
+export default getStoredUsers;
