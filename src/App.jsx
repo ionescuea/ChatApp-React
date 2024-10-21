95d371fb9363177715f2af577d0b6a1b74bf3776
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-10-21 18:18:00 +0100

    Add NavBar to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import NavBar from './components/NavBar';
 import HomePage from './components/pages/HomePage'
 import Register from './components/pages/Register';
@@ -5,23 +6,25 @@ import Login from './components/pages/Login';
 import Chat from './components/pages/Chat';
 import AdminPage from './components/pages/AdminPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { useLocation } from 'react-router-dom';
 
 
 function App() {
-  const handlePageChange = (page) => {
-    window.location.pathname = page;
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const handleLogin = (isAdmin) => {
+    setIsAdmin(isAdmin);
+    localStorage.setItem('isAdmin', isAdmin);
   };
 
   return (
     <Router>
-      <NavBar currentPage={window.location.pathname} handlePageChange={handlePageChange} />
+      <NavBar currentPage={window.location.pathname} isAdmin={isAdmin} />
       <Routes>
         <Route path='/HomePage' element={<HomePage />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login onLogin={handleLogin} />} />
         <Route path='/chat' element={<Chat />} />
-        <Route path='/adminPage' element={<AdminPage />} />
+        <Route path='adminPage' element={<AdminPage />} />
       </Routes>
     </Router>
   )
