443a04e184e7c81badf2b03ec64dae199ec9c29b
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-11-27 13:32:45 +0000

    Design not found page

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <section id="chat">
+  <section id="page-not-found">
     <div className="container">
-      <div className="row align-items-center justify-content-center h-100">
-        <h1>Page Not Found</h1>
+      <div className="square-not-found">
+        <div className="row text-center">
+          <div className="text-overlay">
+            <p className="not-found-text">
+              Sorry, the page you are looking for does not exist
+            </p>
+            <Link to="/">
+              <button className="center-button active">Back to our site</button>
+            </Link>
+          </div>
+          <img src="/logo.jpg" alt="Logo" className="logo"></img>
+        </div>
       </div>
     </div>
-  </section >
+  </section>
 );
 
 export default NotFound;
